2a91187c68413f45e677162dc81dfa6f841e1480
LYX <lyxing9527@163.com>
2025-08-01 18:08:19 +0800

    feat: 优化多平台数据结构，新增rank及num支持

--- shared/weibo.ts
+++ shared/weibo.ts
@@ -1,12 +1,17 @@
 // Power by https://github.com/ourongxing/
-import {proxyPicture} from "../utils";
+import {genRandomUserAgent, proxyPicture} from "../utils";
 import axios from "axios";
 import {WEIBO_API} from "../constant";
 
 
 export const weibo = async () => {
     if (!WEIBO_API) throw new Error("Weibo API is missing");
-    const res: shared.WeiBoRes = (await axios.get(WEIBO_API)).data
+    const res: shared.WeiBoRes = (await axios.get(WEIBO_API,{
+        headers:{
+            "User-Agent": genRandomUserAgent(),
+            "referer": "https://weibo.com/newlogin?tabtype=search&gid=&openLoginLayer=0&url=https%3A%2F%2Fweibo.com%2Fhot%2Fsearch"
+        }
+    })).data
     return res.data.realtime
         .filter(k => !k.is_ad)
         .map((k) => {
@@ -19,6 +24,9 @@ export const weibo = async () => {
                         url: proxyPicture(k.icon),
                         scale: 1.5,
                     },
+                    rank: k.rank,
+                    info: k.word_scheme ? k.word_scheme : "",
+                    num: k.num,
                 },
                 url: `https://s.weibo.com/weibo?q=${encodeURIComponent(keyword)}`,
                 mobileUrl: `https://m.weibo.cn/search?containerid=231522type%3D1%26q%3D${encodeURIComponent(keyword)}&_T_WM=16922097837&v_p=42`,
